4254f2f5ba85135fad9ec1f183cddfd201159176
Geert Lorang <geert@lorang.be>
2021-06-26 00:18:27 +0200

    Add clock

--- app/src/main/java/be/lorang/nuplayer/ui/MainActivity.java
+++ app/src/main/java/be/lorang/nuplayer/ui/MainActivity.java
@@ -21,10 +21,12 @@ package be.lorang.nuplayer.ui;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
@@ -32,6 +34,12 @@ import androidx.leanback.widget.SearchEditText;
 
 import java.net.CookieHandler;
 import java.net.CookieManager;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 import be.lorang.nuplayer.R;
 import be.lorang.nuplayer.services.AuthService;
@@ -41,6 +49,10 @@ public class MainActivity extends FragmentActivity {
 
     public static String PREFERENCES_NAME = "VRTNUPreferences";
     private static String TAG = "MainActivity";
+
+    // Hour formatter
+    private static final DateTimeFormatter hourFormatter = DateTimeFormatter.ofPattern("HH:mm")
+            .withZone(ZoneId.of("Europe/Brussels"));
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,6 +79,35 @@ public class MainActivity extends FragmentActivity {
 
         // Remove expired caches
         HTTPClient.clearExpiredCache(getCacheDir());
+
+        // Setup clock ticker
+        setupClockTicker();
+    }
+
+    private void setupClockTicker() {
+
+        // Set initial value
+        updateClock();
+
+        // Update clock every 60 seconds, calculate initial delay to start of next minute
+        int initialDelay = 60 - LocalDateTime.now().plusMinutes(1).getSecond();
+
+        ScheduledThreadPoolExecutor executor =  new ScheduledThreadPoolExecutor(1);
+        executor.scheduleWithFixedDelay(() -> {
+            updateClock();
+        }, initialDelay, 60L, TimeUnit.SECONDS);
+
+    }
+
+    private void updateClock() {
+
+        // Update clock
+        TextView menuClock = findViewById(R.id.menuClock);
+        if(menuClock != null) {
+            runOnUiThread(() -> {
+                menuClock.setText(hourFormatter.format(Instant.now()));
+            });
+        }
     }
 
     @Override
